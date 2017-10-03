81159175e61612a16068d7f8f2b59ced6ff60de5
sammoosavi <rameenrastanv@hotmail.com>
2017-10-03 16:53:55 -0400

    #3 Added Login Test

---
+++ trndiiapp/tests/Browser/LoginTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Chrome;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * Tests User Login
+     *
+     * @return void
+     */
+    public function testLogin()
+    {
+
+
+        $this->browse(function ($browser) {
+            $browser->visit('/')
+                    ->type('email', 'sammoosavi94@gmail.com')
+                    ->type('password', 'test1234')
+                    ->press('Login')
+                    ->assertSee('You are logged in!');
+        });
+    }
+}
