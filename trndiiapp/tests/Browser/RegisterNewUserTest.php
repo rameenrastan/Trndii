5a7bade3317c721cea508118d57ba4e5dcdd099f
sammoosavi <sammoosavi94@gmail.com>
2017-10-15 15:28:13 -0400

    Added dusk tests for the registration, with @rameenrastan, working on #2

---
+++ trndiiapp/tests/Browser/RegisterNewUserTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class RegisterNewUserTest extends DuskTestCase
+{
+    /**
+     * Tests registration of a new user.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $string = str_random(40);
+            $browser->visit('/register')
+                ->type('name', 'Jason')
+                ->type('email', $string . '@test.com')
+                ->type('password', 'test123')
+                ->type('password_confirmation', 'test123')
+                ->press('Register')
+                ->assertSee('You are logged in!');
+        });
+    }
+}
