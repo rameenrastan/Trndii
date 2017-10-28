2750a4e21c233a261425d1138cbd59fbe3ee4aaa
Eric Payette <e_paye@encs.concordia.ca>
2017-10-28 02:40:18 -0400

    #8 View Item and seeding setup
    - Implemented browsing all items. When clicking on item it redirects user to the items info page.
    - Can populate database with items using php artisan db:seed

---
+++ trndiiapp/resources/views/item/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+    <h1>Items</h1>
+    @if(count($items)>0)
+
+        @foreach($items as $item)
+            <div class="well">
+                <h6>
+                    <a href="item/{{$item->id}}"> {{$item->Name}}</a>
+                </h6>
+            </div>
+            @endforeach
+
+        {{$items->links()}}
+            @else
+                <p>No items in database</p>
+            @endif
+
+@endsection
