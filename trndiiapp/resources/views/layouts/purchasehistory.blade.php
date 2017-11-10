355eb58095181b6fe189c4dd205931b1971c7fb6
sammoosavi <sammoosavi94@gmail.com>
2017-11-10 14:57:40 -0500

    purchase history ui bugfix, with @rameenrastan, working on #42

--- trndiiapp/resources/views/layouts/purchasehistory.blade.php
+++ trndiiapp/resources/views/layouts/purchasehistory.blade.php
@@ -212,6 +212,8 @@
 
         </div>
     </div>
-
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 @endsection
