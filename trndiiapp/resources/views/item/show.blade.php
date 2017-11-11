4addb04b55b102885f8bad75c1f212a0d4561ce9
Jacqueline <luo.jackie@hotmail.com>
2017-11-11 15:42:21 -0500

    #40 Confirmation message is now a modal
    Instead of a browser message box, the confirmation of purchase is now a modal.

--- trndiiapp/resources/views/item/show.blade.php
+++ trndiiapp/resources/views/item/show.blade.php
@@ -8,29 +8,6 @@
 
 @section('content')
 
-    <!--
-    <h1>{{$item->Name}}</h1>
-    <p>Price :{{$item->Price}}</p>
-    <p>Threshold: {{$item->Threshold}}</p>
-    <p>Tokens given: {{$item->Tokens_Given}}</p>
-    <p>Short Description: {{$item->Short_Description}}</p>
-    <p>Long Description: {{$item->Long_Description}}</p>
-    <p>Start Date: {{$item->Start_Date}}</p>
-    <p>End Date: {{$item->End_Date}}</p>
-
-
-    @if($checkCommit == 0 && $item->Threshold > $item->Number_Transaction)
-    {!! Form::open(['action' => ['TransactionsController@update', $item->id], 'method' => 'POST']) !!}
-    {{Form::hidden('_method', 'PUT')}}
-    {{Form::submit('Purchase', ['class' => 'btn btn-primary', 'onclick' => 'return confirm("Are you sure you want to commit to this purchase?");'])}}
-    {!! Form::close() !!}
-    @elseif($item->Threshold <= $item->Number_Transactions)
-        <p>The threshold for this item has already been reached. Sorry!</p>
-    @else
-        <p>You have already commited to this item. Stay tuned!</p>    
-    @endif
-
-    -->
  <div class="container" style="background-color:white ">
      <div class="row">
          <div class="col-md-offset-2 col-md-8">
@@ -59,7 +36,8 @@
                 @endif
             </div>
 
-
+            <!--Progress bar that fills when users commit to the item (and thus Number_Transaction increments) The text is not in the bar because 
+            Bootstrap has issues when the bar is at low % and doesn't display all the text.-->
             <div class="progress" style="margin: 20px;">
                 <div class="progress-bar" role="progressbar" aria-valuenow="70"
                     aria-valuemin="0" aria-valuemax="100" style="width:{{$item->Number_Transactions/$item->Threshold*100}}%; background-color: #14A989;">
@@ -78,22 +56,39 @@
     </div>
     <div class="row">
         <div class="col-md-12" style="text-align: center; margin-bottom: 30px; margin-top: 10px;">
-
-            @if($checkCommit == 0 && $item->Threshold > $item->Number_Transaction)
-                {!! Form::open(['action' => ['TransactionsController@update', $item->id], 'method' => 'POST']) !!}
-                {{Form::hidden('_method', 'PUT')}}
-                {{Form::submit('Purchase', ['class' => 'btn btn-success', 'onclick' => 'return confirm("Are you sure you want to commit to this purchase?");'])}}
-                {!! Form::close() !!}
-            @elseif($item->Threshold <= $item->Number_Transactions)
-                <p>The threshold for this item has already been reached. Sorry!</p>
-            @else
-                <p>You have already commited to this item. Stay tuned!</p>
-            @endif
+         <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#BuyModal">Purchase</button>
         </div>
     </div>
  </div>
 
-
-
+<!--Purchase button. When it is pressed it pops up a modal where user confirms their purchase.-->
+<div id="BuyModal" class="modal fade" aria-labelledby="basicModal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title">Are you sure you want to commit to this purchase?</h3>
+            </div>
+            <div class="modal-body">
+                <p> <strong>Item details: </strong></p>
+                <p>{{$item->Name}} 
+                <br> Price: {{$item->Price}}$ 
+                <br> Tokens Gained: {{$item->Tokens_Given}} </p>
+            </div>
+            <div class="modal-footer">
+                @if($checkCommit == 0 && $item->Threshold > $item->Number_Transaction)
+                    {!! Form::open(['action' => ['TransactionsController@update', $item->id], 'method' => 'POST']) !!}
+                    {{Form::hidden('_method', 'PUT')}}
+                    {{Form::submit('Confirm', ['class' => 'btn btn-primary'])}}
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Return</button>
+                    {!! Form::close() !!}
+                @elseif($item->Threshold <= $item->Number_Transactions)
+                    <p>The threshold for this item has already been reached. Sorry!</p>
+                @else
+                    <p>You have already commited to this item. Stay tuned!</p>
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
