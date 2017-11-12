e59aafc502935567249ad97339b09276b38da6d4
Rameen Rastan <rameenrastanv@hotmail.com>
2017-11-12 14:08:52 -0500

    #12 bugfix: purchase history was showing all users' purchased items

--- trndiiapp/app/Http/Controllers/TransactionsController.php
+++ trndiiapp/app/Http/Controllers/TransactionsController.php
@@ -26,6 +26,7 @@ class TransactionsController extends Controller
         $items = DB::table('items')
                         ->join('transactions', 'items.id', '=', 'transactions.item_fk')
                         ->select('items.id', 'items.Name', 'items.Price', 'items.Bulk_Price', 'items.Short_Description', 'items.Start_Date', 'items.End_Date', 'items.Status', 'items.Threshold', 'items.Number_Transactions', 'items.Status', 'items.Picture_URL', 'transactions.created_at')
+                        ->where('transactions.email', Auth::user()->email)
                         ->orderBy('transactions.created_at', 'DESC')
                         ->get();
 
