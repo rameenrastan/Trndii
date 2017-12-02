6fde1ce55fa9c09312f49809656fcb26a76cc6ea
Eric Payette <e_paye@encs.concordia.ca>
2017-12-02 15:45:36 -0500

    #24 Made item.create more presentable @LordRoost

--- trndiiapp/resources/views/item/create.blade.php
+++ trndiiapp/resources/views/item/create.blade.php
@@ -6,57 +6,106 @@
 
 @section('content')
 
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <h1>Create item</h1>
-            <hr>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Create Item</div>
 
-    {!! Form::open(['route' => 'item.store']) !!}
+                        <div class="panel-body">
 
-    {{Form::label('Name', 'Item Name')}}
-    {{Form::text('Name', '', array('class' => 'form-control'))}}
+                            {!! Form::open(['route' => 'item.store','class'=>'form-horizontal'])!!}
 
-    {{Form::label('Price')}}
-    {{Form::number('Price','0.00', array('step'=>'0.01','class' => 'form-control'))}}
+                            <div class="form-group">
+                                {{Form::label('Name', 'Item Name',array('class'=>'col-md-4 control-label'))}}
+                                <div class="col-md-6">
+                                    {{Form::text('Name', '', array('class' => 'form-control'))}}
+                                </div>
+                            </div>
 
-    {{Form::label('Bulk Price')}}
-    {{Form::number('Bulk Price','0.00', array('step'=>'0.01','class' => 'form-control'))}}
+                            <div class="form-group">
+                                {{Form::label('Price','Price',array('class'=>'col-md-4 control-label'))}}
+                                <div class="col-md-6">
+                                    {{Form::number('Price','0.00', array('step'=>'0.01','class' => 'form-control'))}}
+                                </div>
+                            </div>
 
-    {{Form::label('Tokens Given')}}
-    {{Form::number('Tokens Given', null, array('class' => 'form-control'))}}
+                            <div class="form-group">
+                                {{Form::label('Bulk Price','Bulk Price',array('class'=>'col-md-4 control-label'))}}
+                                <div class="col-md-6">
+                                    {{Form::number('Bulk Price','0.00', array('step'=>'0.01','class' => 'form-control'))}}
+                                </div>
+                            </div>
 
-    {{Form::label('Threshold')}}
-    {{Form::number('Threshold', null, array('class' => 'form-control'))}}
+                            <div class="form-group">
+                                 {{Form::label('Tokens Given','Tokens Given', array('class'=>'col-md-4 control-label'))}}
+                                  <div class="col-md-6">
+                                    {{Form::number('Tokens Given', null, array('class' => 'form-control'))}}
+                                  </div>
+                            </div>
 
-    {{Form::label('Short Description')}}
-    {{Form::text('Short Description', null, array('class' => 'form-control'))}}
+                            <div class="form-group">
+                                {{Form::label('Threshold','Threshold',array('class'=>'col-md-4 control-label'))}}
+                                <div class="col-md-6">
+                                    {{Form::number('Threshold', null, array('class' => 'form-control'))}}
+                                </div>
+                            </div>
 
-    {{Form::label('Long Description')}}
-    {{Form::textarea('Long Description', null, array('class' => 'form-control'))}}
+                            <div class="form-group">
+                                {{Form::label('Short Description','Short Description',array('class'=>'col-md-4 control-label'))}}
+                                <div class="col-md-6">
+                                    {{Form::text('Short Description', null, array('class' => 'form-control'))}}
+                                </div>
+                            </div>
 
-    {{Form::label('Start Date')}}
-    {{Form::date('Start Date', null, array('class' => 'form-control', 'id' => 'start-date'))}}
+                            <div class="form-group">
+                                {{Form::label('Long Description','Long Description',array('class'=>'col-md-4 control-label'))}}
+                                <div class="col-md-6">
+                                    {{Form::textarea('Long Description', null, array('class' => 'form-control','size'=>'30x3'))}}
+                                </div>
+                            </div>
 
-    {{Form::label('End Date')}}
-    {{Form::date('End Date', null, array('class' => 'form-control', 'id' => 'end-date'))}}
+                            <div class="form-group">
+                                {{Form::label('Start Date','Start Date',array('class'=>'col-md-4 control-label'))}}
+                                <div class="col-md-6">
+                                    {{Form::date('Start Date', null, array('class' => 'form-control', 'id' => 'start-date'))}}
+                                </div>
+                            </div>
 
-    {{Form::label('Picture URL')}}
-    {{Form::text('Picture URL', null, array('class' => 'form-control'))}}
+                            <div class="form-group">
+                                {{Form::label('End Date','End Date',array('class'=>'col-md-4 control-label'))}}
+                                <div class="col-md-6">
+                                    {{Form::date('End Date', null, array('class' => 'form-control', 'id' => 'end-date'))}}
+                                </div>
+                            </div>
 
-    <!-- Still work in progress, this is just a very basic implementation-->
-    {{Form::label('Shipping To')}}
-    {{ Form::select('Shipping_To', [
-        'Canada and United States'=>'Canada, United States', 'Canada'=>'Canada', 'United States'=>'United States'
-    ], null, array('class' => 'form-control'))}}
+                            <div class="form-group">
+                                {{Form::label('Picture URL','Picture URL',array('class'=>'col-md-4 control-label'))}}
+                                <div class="col-md-6">
+                                    {{Form::text('Picture URL', null, array('class' => 'form-control'))}}
+                                </div>
+                            </div>
 
-            <p></p>
+                            <div class="form-group">
+                                {{Form::label('Shipping To','Shipping To',array('class'=>'col-md-4 control-label'))}}
+                                <div class="col-md-6">
+                                    {{ Form::select('Shipping_To', ['Canada and United States'=>'Canada, United States', 'Canada'=>'Canada', 'United States'=>'United States'], null, array('class' => 'form-control'))}}
+                                </div>
+                            </div>
 
-    {{Form::submit('Create Item', array('class' => 'btn', 'id' => 'create-item'))}}
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    {{Form::submit('Submit', array('class' => 'btn btn-primary', 'id' => 'create-item'))}}
+                                </div>
+                            </div>
 
-    <p></p>
 
-    {!! Form::close() !!}
+                            {!! Form::close() !!}
 
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
