3b74e31bdbcc4c4348d5d9ba1379d812a26b614a
jason <jtsalikis@hotmail.ca>
2017-12-28 21:50:07 -0500

    flow for from PDF controller to PDF repository now set up, missing now the sql call to get Addresses for an item, working on #28

--- trndiiapp/app/Http/Controllers/PDFController.php
+++ trndiiapp/app/Http/Controllers/PDFController.php
@@ -4,15 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use PDF;
+use App\Repositories\Interfaces\PdfRepositoryInterface;
 
 class PDFController extends Controller
 {
+
+
+    protected $pdfRepo;
+
+    public function _construct(PdfRepositoryInterface $pdfRepo){
+
+        $this->pdfRepo = $pdfRepo;
+    }
+
     public function makePDF()
     {
+
         $data = ['data' => 'Addresses'];
 
         $pdf = PDF::loadView('pdf.addresses', $data);
 
         return $pdf->download('addresses.pdf');
+    }
+
+    public function getPdfByItem(item $item)
+    {
+
+
+        $data = ['data' => $this->pdfRepo->findAddressByItem($item)];
+
+        $pdf = PDF::loadView('pdf.addresses', $data);
+
+        return $pdf->download('addresses.pdf');
+
+
     }
 }
