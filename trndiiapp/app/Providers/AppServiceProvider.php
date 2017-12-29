935f2e06dc7044454579da9e6c3e06f265b1e58a
jason <jtsalikis@hotmail.ca>
2017-12-28 20:16:18 -0500

    added repository template for pdf related calls, for now to be used in pdf controller

--- trndiiapp/app/Providers/AppServiceProvider.php
+++ trndiiapp/app/Providers/AppServiceProvider.php
@@ -8,6 +8,9 @@ use Laravel\Dusk\DuskServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+
+    const INTERFACE_REPOSITORY_DIRECTORY = "App\Repositories\Interfaces";
+    const REPOSITORY_DIRECTORY = "App\Repositories";
     /**
      * Bootstrap any application services.
      *
@@ -29,7 +32,11 @@ class AppServiceProvider extends ServiceProvider
             $this->app->register(DuskServiceProvider::class);
         }
         
-        $this->app->bind('App\Repositories\Interfaces\UserRepositoryInterface', 'App\Repositories\UserRepository');
-        $this->app->bind('App\Repositories\Interfaces\ItemRepositoryInterface', 'App\Repositories\ItemRepository');
+//        $this->app->bind('App\Repositories\Interfaces\UserRepositoryInterface', 'App\Repositories\UserRepository');
+//        $this->app->bind('App\Repositories\Interfaces\ItemRepositoryInterface', 'App\Repositories\ItemRepository');
+//        $this->app->bind('App\Repositories\Interfaces\PdfRepositoryInterface', 'App\Repositories\PdfRepository');
+        $this->app->bind( INTERFACE_REPOSITORY_DIRECTORY+'\UserRepositoryInterface', REPOSITORY_DIRECTORY+'\UserRepository');
+        $this->app->bind(INTERFACE_REPOSITORY_DIRECTORY+'\ItemRepositoryInterface', REPOSITORY_DIRECTORY+'\ItemRepository');
+        $this->app->bind(INTERFACE_REPOSITORY_DIRECTORY+'\PdfRepositoryInterface', REPOSITORY_DIRECTORY+'\PdfRepository');
     }
 }
---
+++ trndiiapp/app/Repositories/PdfRepository.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jason
+ * Date: 2017-12-28
+ * Time: 8:00 PM
+ */
+
+namespace App\Repositories;
+
+
+use App\Repositories\Interfaces\PdfRepositoryInterface;
+
+class PdfRepository implements PdfRepositoryInterface
+{
+
+    public function findAddressByItem($item)
+    {
+        // TODO: Implement findAddressByItem() method.
+
+    }
+}
