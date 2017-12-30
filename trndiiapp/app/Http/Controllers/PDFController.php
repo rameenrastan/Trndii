9934bc4d3de06acd4a440a74392673d97c5dbe80
jason <jtsalikis@hotmail.ca>
2017-12-30 15:28:09 -0500

    working on #28 , formated blade so PDF is formated in a structured way display address information of user

--- trndiiapp/app/Http/Controllers/PDFController.php
+++ trndiiapp/app/Http/Controllers/PDFController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use PDF;
 
 use Illuminate\Support\Facades\DB;
 use App\Repositories\Interfaces\PdfRepositoryInterface as PdfRepositoryInterface;
+use Psy\Util\Json;
 
 class PDFController extends Controller
 {
@@ -30,15 +32,15 @@ class PDFController extends Controller
     }
 
 
-    public function getPdfByItem($itemId)
+    public function getPdfByItem(PdfRepositoryInterface $pdfRepo , $itemId)
     {
 
-
-        $data = ['data' => $this->pdfRepo->findAddressByItemId($itemId)];
+        $data = ['data' => $pdfRepo->findAddressByItemId($itemId)];
 
         $pdf = PDF::loadView('pdf.addresses', $data);
 
         return $pdf->download('addresses.pdf');
+
 
 
     }
@@ -49,12 +51,14 @@ class PDFController extends Controller
 
         $itemId = 1;
 
-
         $data = ['data' => $pdfRepo->findAddressByItemId($itemId)];
 
         $pdf = PDF::loadView('pdf.addresses', $data);
 
-        return $pdf->download('addresses.pdf');
+        $pdfName = $itemId . "Addresses.pdf";
+
+
+        return $pdf->download($pdfName);
 
 
     }
--- trndiiapp/resources/views/pdf/addresses.blade.php
+++ trndiiapp/resources/views/pdf/addresses.blade.php
@@ -1,1 +1,12 @@
-<h1>{{$data}}</h1>
+
+
+@foreach($data as $user)
+    <p>{{ $user->name }} </p>
+    <p>{{ $user->addressline1 }} </p>
+    <p>{{ $user->postalcode }} </p>
+    <p>{{ $user->country }} </p>
+    <br/>
+    <br/>
+@endforeach
+
+
