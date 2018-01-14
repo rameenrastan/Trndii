f4812cd18f600224927510140269cf7577d22c6c
Peter Boulos <peter_04man@hotmail.com>
2018-01-14 11:58:26 -0500

    #64 Fixed all items page display to match browsing categories one.

--- trndiiapp/resources/views/item/index.blade.php
+++ trndiiapp/resources/views/item/index.blade.php
@@ -8,55 +8,64 @@
 row on the right is composed of 3 rows, each row containing part of the item information.--}}
 
 @section('content')
-    <div class="container-fluid">
+
+    <div class="container">
         <div class="row">
-            <div class="col-md-12">
-                <h2> 
-                    <span style='font-weight: bold;'>
-                        Browsing Items <font style="font-size: 20px;">(Total: {{count($items)}})</font>
-                    </span>
-                </h2>
-            </div>
-        </div>
-
-        @if(count($items)>0)
-
-            @foreach($items as $item)
-                
-                <div class="row">
-                    <div class="col-md-2">
-                        <a href="item/{{$item->id}}">
-                            <img alt="{{$item->Name}}" src="{{$item->Picture_URL}}" class="img-thumbnail" />
-                        </a>            
-                    </div>
-                    <div class="col-md-10">
+            <div class="col-md-7 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">All Items</div>
+                    <div class="panel-body">
                         <div class="row">
                             <div class="col-md-12">
-                                <h2 style="margin-top: 5px;">
-                                <a href="item/{{$item->id}}"> {{$item->Name}}</a>
+                                <h2>
+                            <span style='font-weight: bold;'>
+                                Browsing Items <font style="font-size: 20px;">(Total: {{count($items)}})</font>
+                            </span>
                                 </h2>
-                                    {{$item->Short_Description}}
-                                <h3>
-                                    ${{$item->Price}}
-                                </h3>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <h4>
-                                    Receive <strong>{{$item->Tokens_Given}}</strong> tokens upon purchase
-                                </h4>
-                            </div>
-                        </div>
+
+                        @if(count($items)>0)
+
+                            @foreach($items as $item)
+
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <a href="item/{{$item->id}}">
+                                            <img alt="{{$item->Name}}" src="{{$item->Picture_URL}}" class="img-thumbnail"/>
+                                        </a>
+                                    </div>
+                                    <div class="col-md-8" style="text-align: left">
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <h2 style="margin-top: 5px;">
+                                                    <a href="item/{{$item->id}}"> {{$item->Name}}</a>
+                                                </h2>
+                                                {{$item->Short_Description}}
+                                                <h3>
+                                                    ${{$item->Price}}
+                                                </h3>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <h4>
+                                                    Receive <strong>{{$item->Tokens_Given}}</strong> tokens upon purchase
+                                                </h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <br>
+                            @endforeach
+
+                            {{$items->links()}}
+                        @else
+                            <p>No items in database</p>
+                        @endif
                     </div>
                 </div>
-                <br>
-            @endforeach
-
-            {{$items->links()}}
-                @else
-                    <p>No items in database</p>
-                @endif
-
+            </div>
+        </div>
     </div>
 @endsection
