c011da3445cbaf42c3612a4e291a37dfa467eb4a
Jacqueline <luo.jackie@hotmail.com>
2018-01-26 16:08:32 -0500

    #61 Fixed some routing issues @michaelgoldfish
    Added checks for guards in order to properly redirect after login.

--- trndiiapp/app/Http/Middleware/RedirectIfAuthenticated.php
+++ trndiiapp/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,8 +17,24 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+        switch($guard) {
+            case 'admin':
+                if(Auth::guard($guard)->check()) {
+                    return redirect()->route('admin.home');
+                }
+                break;
+
+            case 'supplier':
+                if(Auth::guard($guard)->check()) {
+                    return redirect()->route('supplier.home');
+                }
+                break;    
+
+            default:
+                if(Auth::guard($guard)->check()) {
+                    return redirect('/home');
+                }
+                break;
         }
 
         return $next($request);
