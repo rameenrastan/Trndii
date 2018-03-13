dbc38941c138a81de183b7e354fcf209e36b3a4f
sammoosavi <sammoosavi94@gmail.com>
2018-03-13 17:46:28 -0400

    Added database migration for the reviews table, working on #83

---
+++ trndiiapp/database/migrations/2018_03_13_213705_create_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('item_id');
+            $table->string('user_email');
+            $table->string('supplier_email');
+            $table->integer('rating');
+            $table->string('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
