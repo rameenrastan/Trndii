f15d387e3593511b2781fd0caf69eed348e5f6f7
Eric Payette <e_paye@encs.concordia.ca>
2018-03-15 15:49:46 -0400

    #15 Tokens are added to users when an item reaches the threshold

--- trndiiapp/database/factories/ItemFactory.php
+++ trndiiapp/database/factories/ItemFactory.php
@@ -21,9 +21,9 @@ $factory->define(App\item::class, function (Faker $faker) {
     return [
         'Name' => $faker->domainName,
         'Price'=> $price = $faker->randomFloat(2, 10, 250),
-        'Bulk_Price'=> number_format(max($price / $users = $faker->numberBetween(1, 50), $price / 10), 2),
+        'Bulk_Price'=> $bulk_price=number_format(max($price / $users = $faker->numberBetween(1, 50), $price / 10), 2),
         'Threshold'=>max($users - 5, 5),
-        'tokens_Given'=>0,
+        'tokens_Given'=>intval($price-$bulk_price),
         'Short_Description'=>$faker->words(random_int(3, 5), true),
         'Long_Description' => $faker->paragraphs('3', true),
         'Status' => $faker->randomElement(['pending']),
