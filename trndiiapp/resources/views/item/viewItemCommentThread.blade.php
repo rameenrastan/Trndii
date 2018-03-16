efb82ba491e78deafdc2ebf16e1f09312f393c0c
jason <jtsalikis@hotmail.ca>
2018-03-16 19:50:53 -0400

    handles the logic it is fed from the backend, contract obliged, however needs major styling reworks, working on #91

--- trndiiapp/resources/views/item/viewItemCommentThread.blade.php
+++ trndiiapp/resources/views/item/viewItemCommentThread.blade.php
@@ -1,2 +1,38 @@
-{{$item}}
-{{$user}}
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="col-md-8 col-md-offset-2">
+    @foreach($itemComments as $com)
+        <p>{{ $com->comment }} </p>
+        <p>{{ $com->username }} </p>
+        <br/>
+        <br/>
+    @endforeach
+
+    </div>
+
+
+    <div class="row">
+        <div id ="comment-form"  class="col-md-8 col-md-offset-2">
+
+
+            {{ Form::open(['route' => ['ItemController.addComment', $item->id], 'method' => 'POST']) }}
+
+            <div class="row">
+
+                <div class="col-md-12">
+                    {{ Form::label('comment', "Comment:") }}
+                    {{ Form::textarea('comment', null, ['class' => 'form-control', 'rows' => '5']) }}
+
+                    {{ Form::submit('Add Comment', ['class' => 'btn btn-success btn-block', 'style' => 'margin-top:15px;']) }}
+                </div>
+            </div>
+
+            {{ Form::close() }}
+
+
+
+        </div>
+    </div>
+@endsection
