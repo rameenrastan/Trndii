81f57269896f53dc7751e39e11b3fe544fe885b8
jason <jtsalikis@hotmail.ca>
2018-03-17 15:22:59 -0400

    added comment module to show.blade.php, working on #91

--- trndiiapp/resources/views/item/show.blade.php
+++ trndiiapp/resources/views/item/show.blade.php
@@ -202,4 +202,44 @@
     </div>
 </div>
 
+
+
+
+
+
+
+ <div class="col-md-8 col-md-offset-2">
+     @foreach($itemComments as $com)
+         <p>{{ $com->comment }} </p>
+         <p>{{ $com->username }} </p>
+         <br/>
+         <br/>
+     @endforeach
+
+ </div>
+
+
+ <div class="row">
+     <div id ="comment-form"  class="col-md-8 col-md-offset-2">
+
+
+         {{ Form::open(['route' => ['ItemController.addComment', $item->id, "itemShow"], 'method' => 'POST']) }}
+
+         <div class="row">
+
+             <div class="col-md-12">
+                 {{ Form::label('comment', "Comment:") }}
+                 {{ Form::textarea('comment', null, ['class' => 'form-control', 'rows' => '5']) }}
+
+                 {{ Form::submit('Add Comment', ['class' => 'btn btn-success btn-block', 'style' => 'margin-top:15px;']) }}
+             </div>
+         </div>
+
+         {{ Form::close() }}
+
+
+
+     </div>
+ </div>
+
 @endsection
